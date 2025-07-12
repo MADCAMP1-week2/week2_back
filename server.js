a532fcfb427c3e3252ef6f2b6f9658cb42418b7c
BaeYoona <jinalks43@gmail.com>
2025-07-13 04:18:10 +0900

    feat: add category creation and fetching feature

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./db');
 const authRoutes = require('./routes/authRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/categories', categoryRoutes);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log('서버 시작');
