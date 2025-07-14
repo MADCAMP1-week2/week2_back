62aa7b99deb6587ea862e7097fccef9914ce5a8d
BaeYoona <jinalks43@gmail.com>
2025-07-14 20:13:12 +0900

    feat: delete related todos and schedules when category is removed

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,6 @@
 const Category = require("../models/Category");
+const Schedule = require("../models/Schedule");
+const Todo = require("../models/Todo");
 const asyncHandler = require("../middlewares/asyncHandler");
 
 // 카테고리 생성
@@ -30,6 +32,24 @@ exports.updateCategory = asyncHandler(async (req, res) => {
 // 카테고리 삭제
 exports.deleteCategory = asyncHandler(async (req, res) => {
   const owner = req.user.userId;
-  await Category.findOneAndDelete({ _id: req.params.id, owner });
+  const categoryId = req.params.id;
+
+  // 해당 카테고리 존재 여부 확인
+  const category = await Category.findOne({ _id: categoryId, owner });
+
+  if (!category) {
+    return res.status(404).json({ message: "카테고리를 찾을 수 없습니다." });
+  }
+
+  // 관련 schedule, todo 삭제
+  await Schedule.deleteMany({ category: categoryId });
+  await Todo.deleteMany({ category: categoryId });
+
+  // 카테고리 삭제
+  await Category.findOneAndDelete({
+    _id: categoryId,
+    owner,
+  });
+
   res.status(204).end();
 });
